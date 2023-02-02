07790573be79d1034509609e9d263acfd17d5321
Raul Ramos <hdjsmraul@hotmail.com>
2023-02-02 19:12:55 -0300

    segundo commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+//pre loader
+const loader = document.getElementById('preloader')
+
+window.addEventListener('load', function () {
+    setTimeout(() => {
+
+        loader.style.display = 'none'
+    }, 2000);
+})
